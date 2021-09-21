f5b46355e1fc208b76281747db9d33fd9424a138
Brandonarmand96 <66756596+Brandonarmand96@users.noreply.github.com>
2021-09-21 04:33:03 +0100

    ok

---
+++ components/avatar.js
@@ -0,0 +1,7 @@
+function Avatar({url}){
+    return (
+        <img loading="lazy" src={url} className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110" alt="profile" />
+    )
+}
+
+export default Avatar;
